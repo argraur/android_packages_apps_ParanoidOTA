0b098520afc189f027ed34ab0ba8312b9032a3eb
Arseniy Graur <argraur12@yandex.ru>
2016-12-24 18:58:12 +0300

    com.pixelrom.ota (15/29)

--- src/com/pixelrom/ota/helpers/SettingsHelper.java
+++ src/com/pixelrom/ota/helpers/SettingsHelper.java
@@ -17,7 +17,7 @@
  * along with Paranoid OTA.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.paranoid.paranoidota.helpers;
+package com.pixelrom.ota.helpers;
 
 import android.content.Context;
 import android.content.SharedPreferences;
