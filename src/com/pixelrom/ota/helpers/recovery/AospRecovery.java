ce5a5a85578826eb657173bedf112c58af3422ff
Arseniy Graur <argraur12@yandex.ru>
2017-03-27 16:40:14 +0300

    Remove backups support

--- src/com/pixelrom/ota/helpers/recovery/AospRecovery.java
+++ src/com/pixelrom/ota/helpers/recovery/AospRecovery.java
@@ -45,7 +45,7 @@ public class AospRecovery extends RecoveryInfo {
 
     @Override
     public String[] getCommands(Context context, String[] items, String[] originalItems,
-            boolean wipeData, boolean wipeCaches, String backupFolder, String backupOptions)
+            boolean wipeData, boolean wipeCaches, String backupOptions)
             throws Exception {
 
         List<String> commands = new ArrayList<String>();
@@ -54,33 +54,6 @@ public class AospRecovery extends RecoveryInfo {
 
         boolean hasAndroidSecure = IOUtils.hasAndroidSecure();
         boolean hasSdExt = IOUtils.hasSdExt();
-
-        if (backupFolder != null) {
-            String str = "backup ";
-            if (backupOptions != null && backupOptions.indexOf("S") >= 0) {
-                str += "S";
-            }
-            if (backupOptions != null && backupOptions.indexOf("D") >= 0) {
-                str += "D";
-            }
-            if (backupOptions != null && backupOptions.indexOf("C") >= 0) {
-                str += "C";
-            }
-            if (backupOptions != null && backupOptions.indexOf("R") >= 0) {
-                str += "R";
-            }
-            str += "123";
-            if (backupOptions != null && backupOptions.indexOf("B") >= 0) {
-                str += "B";
-            }
-            if (backupOptions != null && backupOptions.indexOf("A") >= 0 && hasAndroidSecure) {
-                str += "A";
-            }
-            if (backupOptions != null && backupOptions.indexOf("E") >= 0 && hasSdExt) {
-                str += "E";
-            }
-            commands.add(str + "O " + backupFolder);
-        }
         
         commands.add("--set_encrypted_filesystem=on");
         if (wipeData) {
